cbe54a93fff48c05b083d433b37ff65fe0e093c8
James <jb7301722@gmail.com>
2016-03-20 17:58:02 -0500

    add ability to make links active

--- vehicle-tracker/app/controllers/pages_controller.rb
+++ vehicle-tracker/app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
     if user_signed_in?
-      render 'dashboard'
+      redirect_to dashboard_path
     end
   end
 
--- vehicle-tracker/app/helpers/application_helper.rb
+++ vehicle-tracker/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def isActive(controller, action = nil)
+    if action == nil
+      if params[:controller] == controller
+        'active'
+      end
+    else
+      if params[:controller] == controller && params[:action] == action
+        'active'
+      end
+    end
+  end
 end
