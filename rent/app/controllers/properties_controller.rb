b3334edb9aff25c1afa07ec084eaed9da8f91153
James <jb7301722@gmail.com>
2016-03-22 12:30:46 -0500

    allow editing properties

--- rent/app/controllers/properties_controller.rb
+++ rent/app/controllers/properties_controller.rb
@@ -22,6 +22,20 @@ class PropertiesController < ApplicationController
     @property = Property.find(params[:id])
   end
 
+  def edit
+    @property = Property.find(params[:id])
+  end
+
+  def update
+    @property = Property.find(params[:id])
+    if @property.update(property_params)
+      flash[:success] = "Property successfully updated"
+      redirect_to edit_property_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def property_params
     params.require(:property).permit(:address, :city, :state, :zip)
