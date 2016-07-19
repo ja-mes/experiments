2598f4fd3dc3210c613b12faa98eebd1ebd8196f
James <jsbrown000@gmail.com>
2016-07-19 12:57:55 -0500

    Remove advanced seques

---
+++ Random/Advanced Seques/Advanced Seques/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Advanced Seques
+//
+//  Created by James Brown on 12/1/15.
+//  Copyright © 2015 James Brown. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
