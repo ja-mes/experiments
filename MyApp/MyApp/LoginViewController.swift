c3134bc8af14fe70736478f2cf0f6a598f4e53f7
James <jsbrown000@gmail.com>
2016-08-01 07:14:27 -0500

    Add constraints and login view controller

---
+++ MyApp/MyApp/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  MyApp
+//
+//  Created by James Brown on 8/1/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import Cocoa
+
+class LoginViewController: UIViewController {
+
+}
