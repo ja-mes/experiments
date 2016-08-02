a2df36e9ee680cab88513708f89fc3bc3676e50a
James <jsbrown000@gmail.com>
2016-08-02 10:40:39 -0500

    Add learn view controller

---
+++ MyApp/MyApp/LearnViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LearnViewController.swift
+//  MyApp
+//
+//  Created by James Brown on 8/2/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class LearnViewController: UIViewController {
+
+}
