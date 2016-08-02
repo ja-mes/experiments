cfe012fd4ac4b60ac53926f265685b56092fa471
James <jsbrown000@gmail.com>
2016-08-02 13:38:37 -0500

    Add stories table view controller

---
+++ MyApp/MyApp/StoriesTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StoriesTableViewController.swift
+//  MyApp
+//
+//  Created by James Brown on 8/2/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import Cocoa
+
+class StoriesTableViewController: UITableViewController {
+
+}
