d15f329b3599f2665a7622047e73a5b9ea3e851e
James <jsbrown000@gmail.com>
2016-08-02 11:32:33 -0500

    Add animation code and navigation view controller

--- MyApp/MyApp/LearnViewController.swift
+++ MyApp/MyApp/LearnViewController.swift
@@ -9,5 +9,32 @@
 import UIKit
 
 class LearnViewController: UIViewController {
-
+    @IBOutlet var dialogView: DesignableView!
+    @IBOutlet var bookImageView: SpringImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        dialogView.animate()
+    }
+    
+    @IBAction func learnButtonDidTouch(sender: AnyObject) {
+        bookImageView.animation = "pop"
+        bookImageView.animate()
+    }
+    
+    @IBAction func closeButtonDidTouch(sender: AnyObject) {
+        dialogView.animation = "fall"
+        dialogView.animateNext {
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
 }
