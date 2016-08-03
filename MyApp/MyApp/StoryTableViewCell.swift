b4cce37c05741292c2ca2c1eeee867234b449b53
James <jsbrown000@gmail.com>
2016-08-03 06:43:22 -0500

    Add story table view cell

---
+++ MyApp/MyApp/StoryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  StoryTableViewCell.swift
+//  MyApp
+//
+//  Created by James Brown on 8/3/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+  
+
+}
