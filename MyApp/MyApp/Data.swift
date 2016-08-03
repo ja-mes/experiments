4a0454a968061d83019b6ba80d1bfebbb0cf9805
James <jsbrown000@gmail.com>
2016-08-03 11:02:28 -0500

    Add data

--- MyApp/MyApp/Data.swift
+++ MyApp/MyApp/Data.swift
@@ -8,6 +8,40 @@
 
 import UIKit
 
-class Data: NSObject {
-
-}
+var data: [[String:AnyObject]] = [
+    [
+        "id": 72536,
+        "title": "AMA: Dan Makoski, Chief Design Officer at Garage Partners (Formerly at Head of Design at Google, Founded Project Ara)",
+        "comment": "I'm Dan - a [designer](http://www.danmakoski.com/), [maker](https://www.youtube.com/watch?v=aSSuIceLP2g#t=01m08s\u0026rel=0), [speaker](http://tedxtalks.ted.com/video/Selfless-Design-%7C-Daniel-Makosk), [banker](http://www.fastcodesign.com/3032630/why-one-of-googles-wildest-designers-left-for-a-bank), [advisor](http://www.garagepartners.com/), [solopreneur](https://medium.com/mobile-lifestyle/top-10-tools-for-the-solopreneur-355b91fa52aa#.sp46h23sb) and as of Monday - a [crowdfunding entrepreneur](http://venturebeat.com/2016/08/01/googles-project-ara-founder-creates-a-smart-lamp-to-teach-you-better-habits/). I live in Silicon Valley, love the show Silicon Valley, but get easily tired of the superficiality and hype in Silicon Valley. My latest focus is to ask the question, **\"how can technology encourage us to become better people?\"**\r\n\r\nThis is my first AMA, and can't wait to see what is on everyone's minds! I'll answer this, as I'm sure it will be asked - yes, I actually do know seven versions of the running man.\r\n\r\n**I\u2019ll be answering your questions on starting at 10:00 AM PT**",
+        "comment_html": "\u003Cp\u003EI\u0026#39;m Dan - a \u003Ca href=\"http://www.danmakoski.com/\" rel=\"nofollow\"\u003Edesigner\u003C/a\u003E, \u003Ca href=\"https://www.youtube.com/watch?v=aSSuIceLP2g#t=01m08s\u0026amp;rel=0\" rel=\"nofollow\"\u003Emaker\u003C/a\u003E, \u003Ca href=\"http://tedxtalks.ted.com/video/Selfless-Design-%7C-Daniel-Makosk\" rel=\"nofollow\"\u003Espeaker\u003C/a\u003E, \u003Ca href=\"http://www.fastcodesign.com/3032630/why-one-of-googles-wildest-designers-left-for-a-bank\" rel=\"nofollow\"\u003Ebanker\u003C/a\u003E, \u003Ca href=\"http://www.garagepartners.com/\" rel=\"nofollow\"\u003Eadvisor\u003C/a\u003E, \u003Ca href=\"https://medium.com/mobile-lifestyle/top-10-tools-for-the-solopreneur-355b91fa52aa#.sp46h23sb\" rel=\"nofollow\"\u003Esolopreneur\u003C/a\u003E and as of Monday - a \u003Ca href=\"http://venturebeat.com/2016/08/01/googles-project-ara-founder-creates-a-smart-lamp-to-teach-you-better-habits/\" rel=\"nofollow\"\u003Ecrowdfunding entrepreneur\u003C/a\u003E. I live in Silicon Valley, love the show Silicon Valley, but get easily tired of the superficiality and hype in Silicon Valley. My latest focus is to ask the question, \u003Cstrong\u003E\u0026quot;how can technology encourage us to become better people?\u0026quot;\u003C/strong\u003E\u003C/p\u003E\n\n\u003Cp\u003EThis is my first AMA, and can\u0026#39;t wait to see what is on everyone\u0026#39;s minds! I\u0026#39;ll answer this, as I\u0026#39;m sure it will be asked - yes, I actually do know seven versions of the running man.\u003C/p\u003E\n\n\u003Cp\u003E\u003Cstrong\u003EI\u2019ll be answering your questions on starting at 10:00 AM PT\u003C/strong\u003E\u003C/p\u003E\n",
+        "comment_count": 1,
+        "vote_count": 2,
+        "created_at": "2016-08-03T12:44:52Z",
+        "pinned_at": "2016-08-03T15:39:05Z",
+        "url": "https://www.designernews.co/click/stories/72536",
+        "site_url": "https://api.designernews.co/stories/72536-ama-dan-makoski-chief-design-officer-at-garage-partners-formerly-at-head-of-design-at-google-founded-project-ara",
+        "user_id": 1709,
+        "user_display_name": "Maxwell L.",
+        "user_portrait_url": "https://designer-news-production.s3.amazonaws.com/rendered_portraits/1709/original/portrait-2016-03-10_16_56_05__0000-170920160310-3-ke1q1j.png?AWSAccessKeyId=AKIAIBH3R762QIPXF4IQ\u0026Expires=1501796369\u0026Signature=qtzUrYSC0u7EBK4%2FzqZSIfrXO7w%3D",
+        "hostname": "",
+        "user_url": "http://www.designernews.co/users/1709/maxwell-lind",
+        "badge": "pinned",
+        "user_job": "Community Manager at Designer News",
+        "sponsored": false,
+        "comments": [
+            [
+                "id": 1,
+                "body": "value"
+            ],
+            [
+                "id": 2,
+                "body": "value",
+            ]
+        ]
+        
+    ],
+    [
+        "id": 2,
+        "title": "value",
+    ]
+]
