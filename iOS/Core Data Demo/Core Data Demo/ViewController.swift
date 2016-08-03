3cc865b54c4473f676145245404a8b46573139af
James <jsbrown000@gmail.com>
2016-08-03 18:26:16 -0500

    Add core data code

--- iOS/Core Data Demo/Core Data Demo/ViewController.swift
+++ iOS/Core Data Demo/Core Data Demo/ViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        newUser.setValue("James", forKey: "username")
+        newUser.setValue("foo", forKey: "password")
+        
+        do {
+            try context.save()
+        } catch {
+            print("Unable to save")
+        }
+        
+        let request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.executeFetchRequest(request)
+            print(results)
+        } catch {
+            print("Unable to find")
+        }
+        
     }
 
 
