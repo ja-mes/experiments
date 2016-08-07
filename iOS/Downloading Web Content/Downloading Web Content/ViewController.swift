090ecf65e06395664f0da464a6534874c84acb9b
James <jsbrown000@gmail.com>
2016-08-06 20:02:53 -0500

    Updates to whats the weather app

--- iOS/Downloading Web Content/Downloading Web Content/ViewController.swift
+++ iOS/Downloading Web Content/Downloading Web Content/ViewController.swift
@@ -18,23 +18,23 @@ class ViewController: UIViewController {
         
         webView.loadRequest(NSURLRequest(URL: url))
         
-//        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
-//            // will happen when task completes
-//            
-//            if let urlContent = data {
-//                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-//                
-//                dispatch_async(dispatch_get_main_queue(), { 
-//                  self.webView.loadHTMLString(String(webContent!), baseURL: nil)
-//                })
-//                
-//            } else {
-//                // show error message
-//                
-//            }
-//        }
-//        
-//        task.resume()
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            // will happen when task completes
+            
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                dispatch_async(dispatch_get_main_queue(), { 
+                  self.webView.loadHTMLString(String(webContent!), baseURL: nil)
+                })
+                
+            } else {
+                // show error message
+                
+            }
+        }
+        
+        task.resume()
         
     }
 
--- iOS/Whats the Weather/Whats the Weather/ViewController.swift
+++ iOS/Whats the Weather/Whats the Weather/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var city: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func lookupWeather(sender: AnyObject) {
+        //let url = NSURL(fileURLWithPath: "http://www.weather-forcast.com/locations/" + city.text! + "/forcasts/latest")
+//        let url = NSURL(fileURLWithPath: "https://www.google.com")
+//        
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+//            
+//            if let urlContent = data {
+//                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+//                
+//                dispatch_async(dispatch_get_main_queue(), {
+//                    print(webContent)
+//                })
+//            }
+//        }
+//        
+//        task.resume()
+//        
+        
+        let url = NSURL(string: "http://www.weather-forcast.com/locations/Paris/forcasts/latest")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            // will happen when task completes
+            
+            if let urlContent = data {
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                print(webContent)
+                
+            } else {
+                // show error message
+                
+            }
+        }
+        
+        task.resume()
+        
 
+    }
 
 }
 
