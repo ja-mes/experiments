3ff4f2f600691905a2a581d26680dde8fd7535b5
James <jsbrown000@gmail.com>
2016-08-10 15:49:30 -0500

    Create playground

---
+++ iOS/Swift/Objects and classes.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Vehicle {
+    var tires = 4
+    var headlights = 2
+    var horsepower = 468
+    
+    func drive() {
+        //accelerate the vehicle
+    }
+    
+    func brake() {
+        
+    }
+}
