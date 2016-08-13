0195b6f5b70762f65b8f42b9dfe8b84d73688dd1
James <jsbrown000@gmail.com>
2016-08-12 20:18:32 -0500

    Create swaping screens

---
+++ iOS/Swaping screens/Swaping screens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swaping screens
+//
+//  Created by James Brown on 8/12/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
