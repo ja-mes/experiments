23fe91e5208fd35bed0be3a9f3158057027e4bb8
James <jsbrown000@gmail.com>
2016-09-07 20:02:41 -0500

    items

--- Old/MyPlayground.playground/Contents.swift
+++ Old/MyPlayground.playground/Contents.swift
@@ -4,8 +4,26 @@ import UIKit
 
 var str = "Hello, playground"
 
-log2(5.0)
-log(2.0)
+let calculations: [[String:String]] = [
+    [
+        "unit1": "meters",
+        "unit2": "feet",
+        "formula1": "unit1 * 3.2808",
+        "formula2": "unit2 / 3.2808"
+    ],
+    [
+        "unit1": "centimeters",
+        "unit2": "inches",
+        "formula1": "unit1 * 0.39370",
+        "formula2": "unit2 / 0.39370"
+    ]
+]
+
+let favorites = 
+
+
+let item = calculations[0]
+
 
 
 
---
+++ scrollviewerrortest/scrollviewerrortest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scrollviewerrortest
+//
+//  Created by James Brown on 9/5/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
